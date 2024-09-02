47e279091642b36c96a4b9bbe096299cdec58e4d
Akshayk05 <work.akshay05@gmail.com>
2024-09-02 22:27:30 +0530

    1st day of working on a new basic project

---
+++ count_incr_decr/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex flex-col justify-center mt-52 '>
+        <div className='text-4xl text-center font-sans font-semibold mx-2'>
+          Counter
+        </div>
+        <div className='text-5xl text-center font-semibold my-8 mx-2'>
+          {count}
+        </div>
+      </div>
+      <div className='flex flex-wrap justify-center my-8'>
+        <button className='border-2
+         border-black 
+         ml-6 px-6 rounded-lg 
+         shadow-black 
+         shadow-sm 
+         hover:bg-slate-100 
+          transform transition-transform duration-500 hover:scale-110 ease-in-out
+          ' >
+          Increase
+        </button>
+        <button className=' border-2 border-black mx-6 px-6 rounded-lg shadow-black shadow-sm hover:bg-slate-100 ease-in-out transform transition-transform duration-500 hover:scale-110 '>
+          Reset
+        </button>
+        <button className='border-2 border-black px-6 rounded-lg shadow-black shadow-sm hover:bg-slate-100 transform transition-transform duration-500 hover:scale-110 ease-in-out'>
+          Decrease
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
